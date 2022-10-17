48008652e878efa64724185094bb39235f7ae075
jeongmino <jeongmino1207@gmail.com>
2022-10-17 19:56:00 +0900

    plus_making

--- team_proect_1/calculate_num.c
+++ team_proect_1/calculate_num.c
@@ -6,21 +6,21 @@
 /*   By: junoh <junoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:24:10 by junoh             #+#    #+#             */
-/*   Updated: 2022/10/17 11:27:16 by junoh            ###   ########.fr       */
+/*   Updated: 2022/10/17 12:11:54 by junoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./beyond.h"
+// #include "./beyond.h"
 
 
-int calculate_num(t_info *dest, t_info *src, t_info *output)
-{
-    if (operand == PLUS)
-        plus_num(dest, src, output);
-    else if (operand == MINUS)
-        minus_num();
-    else if (operand == MULTIPLY)
-        multiply_num();
-    else
-        divide_num();
-}
+// int calculate_num(t_info *dest, t_info *src, t_info *output)
+// {
+//     if (operand == PLUS)
+//         plus_num(dest, src, output);
+//     else if (operand == MINUS)
+//         minus_num();
+//     else if (operand == MULTIPLY)
+//         multiply_num();
+//     else
+//         divide_num();
+// }
